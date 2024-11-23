42fa89ecd72286aa1face789f5a00b5f3043d0db
ITrue-dev <den.glushkov.kv@gmail.com>
2024-11-23 21:12:42 +0200

    first

--- Exercises/1-callback.js
+++ Exercises/1-callback.js
@@ -1,5 +1,14 @@
 'use strict';
 
-const iterate = (obj, callback) => null;
+const iterate = (obj, callback) => {
+
+    const data = Object.keys(obj);
+    for(const key of data)
+    {
+        const value = obj[key];
+        callback(key, value, obj);
+    }
+
+};
 
 module.exports = { iterate };
--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,24 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+const contract = (fn, ...types) => (...args) => {
+    for(let i = 0; i < args.length; i++)
+    {
+        const arg = args[i];
+        const spic = types[i];
+        const name = spic.name.toLowerCase();
+        if(typeof arg !== name) 
+        {
+            throw new TypeError(`Type ${name} expected`);
+        }
+    }
+    const res = fn(...args);
+    const spic = types[types.length - 1];
+    const name = spic.name.toLowerCase();
+    if(typeof res !== name)
+    {
+        throw new TypeError(`Type ${name} expected`);
+    }
+    return res;
+};
 
 module.exports = { contract };
